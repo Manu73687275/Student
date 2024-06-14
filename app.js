794507cfce9f9e5b5f0d38789c81eff618493ec4
Manu G M <manugmahadeva@gmail.com>
2024-06-14 13:40:30 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -24,10 +24,9 @@ const connectToDB = () => {
         console.log("No mongoURI found");
     }
 };
-
-app.get("/",(req,res)=>{
-    app.use(express.static(path.join(__dirname,"./frontend/dist")));
-    res.sendFile(path.join(__dirname,"./frontend/dist/index.html"));
+app.use(express.static(path.join(__dirname,"frontend/dist")));
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend/dist/index.html"));
 })
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
